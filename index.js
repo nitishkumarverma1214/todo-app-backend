4d55669bf14edf7707eed2b5dade48cd3e4dfec4
Nitish <rdpara1214@gmail.com>
2022-12-14 10:28:53 +0530

    updated portno.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1/", router);
 
-const PORT = 1327;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("app is running at port=", PORT);
 });
